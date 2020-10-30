12b28cb69e6989fc2a15d843cc6be4260d518f51
adam-rappaport <adamrappaport@icloud.com>
2020-10-30 12:33:08 -0500

    finished

--- client/src/components/CheckoutForm.js
+++ client/src/components/CheckoutForm.js
@@ -1,30 +1,14 @@
 import React, { useState } from "react";
+import { useForm } from '../hooks/useForm'
 
-const initialValue = {
-  firstName: "",
-  lastName: "",
-  address: "",
-  city: "",
-  state: "",
-  zip: "",
-};
 
 // This form should be handled by a "useForm" custom hook
 // Build out the logic needed for a form custom hook (see the useForm.js file)
 // and replace the necessary stateful logic from CheckoutForm with the hook
 
 const CheckoutForm = (props) => {
-  const [showSuccessMessage, setShowSuccessMessage] = useState(false);
-  const [values, setValues] = useState(initialValue);
 
-  const handleChanges = (e) => {
-    setValues({ ...values, [e.target.name]: e.target.value });
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    setShowSuccessMessage(true);
-  };
+  const [values,showSuccessMessage,handleChanges,handleSubmit]= useForm();
 
   return (
     <>
@@ -36,6 +20,7 @@ const CheckoutForm = (props) => {
             name="firstName"
             value={values.firstName}
             onChange={handleChanges}
+           
           />
         </label>
         <label>
@@ -44,6 +29,7 @@ const CheckoutForm = (props) => {
             name="lastName"
             value={values.lastName}
             onChange={handleChanges}
+            
           />
         </label>
         <label>
@@ -52,19 +38,20 @@ const CheckoutForm = (props) => {
             name="address"
             value={values.address}
             onChange={handleChanges}
+            
           />
         </label>
         <label>
           City:
-          <input name="city" value={values.city} onChange={handleChanges} />
+          <input name="city" value={values.city} onChange={handleChanges}  />
         </label>
         <label>
           State:
-          <input name="state" value={values.state} onChange={handleChanges} />
+          <input name="state" value={values.state} onChange={handleChanges}   />
         </label>
         <label>
           Zip:
-          <input name="zip" value={values.zip} onChange={handleChanges} />
+          <input name="zip" value={values.zip} onChange={handleChanges}  />
         </label>
         <button>Checkout</button>
       </form>
--- client/src/components/CheckoutForm.test.js
+++ client/src/components/CheckoutForm.test.js
@@ -1,9 +1,44 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import CheckoutForm from "./CheckoutForm";
 
 // Write up the two tests here and make sure they are testing what the title shows
 
-test("form header renders", () => {});
+test("form header renders", () => {
 
-test("form shows success message on submit with form details", () => {});
+    render(<CheckoutForm />);
+    const header = screen.getByText(/checkout form/i);
+  
+    expect(header).toBeInTheDocument();
+});
+
+test("form shows success message on submit with form details", () => {
+    render(<CheckoutForm />);
+  
+    // const firstNameInput1 = render.getByLabelText(/firstName/i);
+    // const lastNameInput1 = render.getByLabelText(/lastName/i);
+    // const addressInput1 = render.getByLabelText(/address/i);
+    // const cityInput1 = render.getByLabelText(/city/i);
+    // const stateInput1 = render.getByLabelText(/state/i);
+    // const zipInput1 = render.getByLabelText(/zip/i);
+
+
+
+    const firstNameInput = screen.getByLabelText(/first name/i);
+    const lastNameInput = screen.getByLabelText(/last name/i);
+    const addressInput = screen.getByLabelText(/address/i);
+    const cityInput = screen.getByLabelText(/city/i);
+    const stateInput = screen.getByLabelText(/state/i);
+    const zipInput = screen.getByLabelText(/zip/i);
+  
+    fireEvent.change(firstNameInput, { target: { value: 'itachi' } });
+    fireEvent.change(lastNameInput, { target: { value: 'uchiha' } });
+    fireEvent.change(addressInput, { target: { value: '555 leaf village' } });
+    fireEvent.change(cityInput, { target: { value: 'kanoha' } });
+    fireEvent.change(stateInput, { target: { value: 'japan' } });
+    fireEvent.change(zipInput, { target: { value: '13579' } });
+    
+    
+   
+
+});
